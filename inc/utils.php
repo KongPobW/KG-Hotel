17819d2a83a64a3a6b8436fdfceb0c6f6eb439eb
kongpob <kongpob.wisitsak@gmail.com>
2025-04-28 14:17:33 +0700

    alert appear for 3 seconds

--- inc/utils.php
+++ inc/utils.php
@@ -4,7 +4,20 @@ function alert($type, $msg) {
     <div class='alert alert-$type alert-dismissible fade show custom-alert' role='alert'>
         <strong>$msg</strong>
         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-    </div>";
+    </div>
+    <script>
+    setTimeout(function() {
+        var alert = document.querySelector('.custom-alert');
+        if(alert) {
+            alert.classList.remove('show');
+            alert.classList.add('hide');
+            setTimeout(function() {
+                alert.remove();
+            }, 500);
+        }
+    }, 3000);
+    </script>
+    ";
 }
 
 function redirect($url) {
