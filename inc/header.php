4cfd8c6cc9604b63e3770dff28f2b9a077f69088
kongpob <kongpob.wisitsak@gmail.com>
2025-07-07 13:45:39 +0700

    make profile dropdown

--- inc/header.php
+++ inc/header.php
@@ -26,12 +26,29 @@
             <?php session_start(); ?>
             <div class="d-flex align-items-center">
                 <?php if (isset($_SESSION['user_id'])): ?>
-                <div class="d-flex align-items-center me-3">
-                    <img src="<?= 'uploads/profiles/' . $_SESSION['profile'] ?>" class="rounded-circle me-3"
-                        style="width: 40px; height: 40px; object-fit: cover;">
-                    <span class="fw-bold"><?= htmlspecialchars($_SESSION['user_name']) ?></span>
+                <div class="dropdown mb-2">
+                    <a class="d-flex align-items-center text-decoration-none dropdown-toggle text-dark" href="#"
+                        id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="<?= 'uploads/profiles/' . $_SESSION['profile'] ?>" class="rounded-circle me-3"
+                            style="width: 40px; height: 40px; object-fit: cover;">
+                        <span class="fw-bold"><?= htmlspecialchars($_SESSION['user_name']) ?></span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end mt-1" aria-labelledby="profileDropdown">
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="#">
+                                <i class="bi bi-calendar-check me-2"></i> My Booking
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item d-flex align-items-center" href="/logout.php">
+                                <i class="bi bi-box-arrow-right me-2"></i> Logout
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <a href="logout.php" class="btn btn-outline-danger shadow-none">Logout</a>
                 <?php else: ?>
                 <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal"
                     data-bs-target="#loginModal">
